75c305956c3127b3aa404541271763f1c7292261
da99 <i-hate-spam-1234567@mailinator.com>
2016-12-09 01:32:55 -0600

    Fixes for VoidLinux.

---
+++ bin/public/compile-node/_.sh
@@ -0,0 +1,60 @@
+
+# === {{CMD}}
+# === {{CMD}}  PREFIX
+compile-node () {
+  if [[ -z "$@" ]]; then
+    export PREFIX="/progs/node"
+  else
+    export PREFIX="$1"; shift
+  fi
+
+  local ARCH="$(lsb_release --codename | cut -f2)"
+
+  # === Install computer packages:
+  if [[ "$ARCH" == "void" ]]; then
+    echo '=== Installing packages:'
+    sudo xbps-install -S \
+      zlib-devel        python-devel icu-devel \
+      libressl-devel    libuv-devel \
+      http-parser-devel python || {
+        STAT=$?
+        if [[ "$STAT" != 6 ]]; then
+          exit $STAT
+        fi
+      }
+  fi # === if $ARCH
+
+  mkdir -p "$PREFIX"
+  cd "$PREFIX"
+  if [[ -d node-src ]]; then
+    cd node-src
+    git checkout master
+    git pull
+  else
+    git clone https://github.com/nodejs/node node-src
+    cd node-src
+  fi
+
+  local +x LATEST=$(git tag | grep -P '^v\d+\.\d+\.\d+$' | sort -V -r | head -n 1)
+
+  git checkout "$LATEST"
+  PREFIX="$PREFIX/$LATEST"
+
+  if [[ -s "$PREFIX/bin/node" ]]; then
+    echo "=== Already installed: $("$PREFIX"/bin/node -v)"
+    return 0
+  fi
+
+  ./configure \
+    --prefix="$PREFIX"   \
+    --with-intl=system-icu \
+    --shared-zlib        \
+		--shared-http-parser \
+		--shared-libuv
+
+  make LDFLAGS+=-ldl
+  make LDFLAGS+=-ldl install
+
+  "$PREFIX"/bin/node -v
+} # === end function
+
--- bin/public/install-node/_.sh
+++ bin/public/install-node/_.sh
@@ -1,24 +1,45 @@
 
-# === {{CMD}} ...
-install-node () {
-  local PREFIX="/progs/node"
+source "$THIS_DIR"/bin/public/compile-node/_.sh
 
+# === {{CMD}}
+# === {{CMD}}  PREFIX
+install-node () {
+  if [[ -z "$@" ]]; then
+    export PREFIX="/progs/node-current"
+  else
+    export PREFIX="$1"; shift
+  fi
+
+  local DESTDIR="$(mktemp -d || echo "/tmp/node-install-$(date '+%M-%D-%S-$H')")"
+
+  local ARCH="$(lsb_release --codename | cut -f2)"
+
+  # === Install computer packages:
+  if [[ "$ARCH" == "void" ]]; then
+    sudo xbps-install -S nodejs || {
+      local +x STAT="$?"
+      if [[ ! "$STAT" -eq 6 ]]; then
+        exit $?
+      fi
+    }
+    return
+  fi # === if $ARCH
 
   # === install  # Installs: node_build, node
 
   # === install: node_build
   cd /progs
-  echo -n "=== Installing/updating $(sh_color colorize YELLOW node-build): "
+  echo -n "=== Installing/updating $(sh_color YELLOW node-build): "
   git_setup clone-or-pull https://github.com/nodenv/node-build /progs/
   node_build="/progs/node-build/bin/node-build"
 
   # === install: node
-  mkdir -p /progs/node/
+  mkdir -p "$PREFIX"
   latest="$($THIS_SCRIPT latest)"
 
-  if [[ ! -d /progs/node/$latest ]]; then
+  if [[ ! -d "$PREFIX"/$latest ]]; then
     echo "=== Installing node: $latest"
-    $node_build "$latest" /progs/node/$latest
+    $node_build "$latest" "$PREFIX"/$latest
   fi
 
   MSG="Already installed:"
@@ -30,18 +51,18 @@ install-node () {
     ln -s /progs/node/$latest "$current"
     packages="jshint npm-check-updates bower eslint"
     npm install -g $packages
-    echo -n "=== Installing $(sh_color colorize YELLOW $packages)... "
+    echo -n "=== Installing $(sh_color YELLOW $packages)... "
     MSG="Installed:"
   fi
 
-  final="$(type node)"
-  if [[ "$final" != */progs/node/current/* ]]; then
-    echo "!!! Something went wrong: $final" 1>&2
+  if [[ "$(type node)" != *"$PREFIX/"* ]] || ! node -v ; then
+    echo "!!! Something went wrong" 1>&2
     exit 1
   fi
 
-  echo "=== $MSG: $(sh_color colorize YELLOW "$(node -v)") and $final"
-  exit 0
+  sh_color  YELLOW  "=== $MSG: {{$(node -v)}}"
+  return 0
+
   # === $ ... install VERSION
   # === Uses NVM to install a node version.
   # $0 nvm install $@
@@ -65,4 +86,5 @@ install-node () {
     sh_color BOLD "=== Trashing: {{$OLD}}"
     trash-put "$PREFIX/$OLD"
   fi
+
 } # === end function
